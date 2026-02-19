c50381b4fd448639c3b5be463c2ea142f802c443
Michael Krebs <michael.krebs@fabasoft.com>
2026-02-19 18:32:06 +0100

    refactor

--- src/main/java/dev/mkrebs/worker/TrivyWorker.java
+++ src/main/java/dev/mkrebs/worker/TrivyWorker.java
@@ -16,7 +16,7 @@ public class TrivyWorker {
     private final WorkerExecutor executor;
 
     TrivyWorker(Vertx vertx) {
-        executor = vertx.createSharedWorkerExecutor("trivy-scanner");
+        executor = vertx.createSharedWorkerExecutor("trivy-scanner", 4);
     }
 
     void tearDown(@Observes ShutdownEvent event) {
@@ -25,12 +25,23 @@ public class TrivyWorker {
 
     public void scanDocker(String image) {
         LOGGER.info("Received Trivy Scan event");
-        try {
-            LOGGER.info(String.format("Scanning %s image", image));
-            Thread.sleep(5000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        LOGGER.info("End long running trivy image scanning event");
+        executor.executeBlocking(() -> {
+            try {
+                LOGGER.infof("Worker thread: %s", Thread.currentThread().getName());
+                LOGGER.info(String.format("Scanning %s image", image));
+                Thread.sleep(5000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LOGGER.info("End long running trivy image scanning event");
+            return "something";
+        }, false)
+                .subscribe().with(
+                        item -> {
+                            System.out.println(item);
+                        },
+                        failure -> {
+                            System.out.println(failure);
+                        });
     }
 }
